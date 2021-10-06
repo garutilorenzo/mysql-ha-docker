d978b6a44d4e8f29018230884923fa77bd170859
Lorenzo Garuti <garuti.lorenzo@gmail.com>
2021-10-06 16:47:34 +0200

    Fix indentation

--- heartbeat/docker-entrypoint.sh
+++ heartbeat/docker-entrypoint.sh
@@ -21,54 +21,54 @@ file_env() {
 }
 
 if [ "$1" = 'pt-heartbeat' ]; then
-  file_env 'PT_DATABASE'
-  if [ -z "$PT_DATABASE" ]; then
-    echo "you have to specify a DB!"
-    exit 1
-  fi
-  file_env 'PT_MASTER'
-  if [ -z "$PT_MASTER" ]; then
-    echo "you have to specify a MySQL Server!"
-    exit 1
-  fi
+	file_env 'PT_DATABASE'
+	if [ -z "$PT_DATABASE" ]; then
+		echo "you have to specify a DB!"
+		exit 1
+	fi
+	file_env 'PT_MASTER'
+	if [ -z "$PT_MASTER" ]; then
+		echo "you have to specify a MySQL Server!"
+		exit 1
+	fi
 	file_env 'PT_USER'
-  if [ -z "$PT_USER" ]; then
-    echo "you have to specify a user!"
-    exit 1
-  fi
+	if [ -z "$PT_USER" ]; then
+		echo "you have to specify a user!"
+		exit 1
+	fi
 	file_env 'PT_PASSWORD'
-  if [ -z "$PT_PASSWORD" ]; then
-    echo "you have to specify a password!"
-    exit 1
-  fi
-  
-  echo "Waiting for DB init..."
-  sleep 30
+	if [ -z "$PT_PASSWORD" ]; then
+		echo "you have to specify a password!"
+		exit 1
+	fi
+	
+	echo "Waiting for DB init..."
+	sleep 30
 
-  file_env 'PT_SERVERS'
-  if [  ! -z "$PT_SERVERS" ]; then
-    echo "Waiting MySQL Servers..."
-    SERVERS=($(echo "$PT_SERVERS" | tr ',' '\n'))
-    for i in "${!SERVERS[@]}"
-    do
-      echo "Waiting for ${SERVERS[i]}..."
-      until mysql --no-defaults -h "${SERVERS[i]}" -u "$PT_USER" -p"$PT_PASSWORD" "$PT_DATABASE" -nsLNE -e 'exit'; do
-        >&2 echo "MySQL Master is unavailable - sleeping"
-        sleep 5
-      done
-    done
-  fi
+	file_env 'PT_SERVERS'
+	if [  ! -z "$PT_SERVERS" ]; then
+		echo "Waiting MySQL Servers..."
+		SERVERS=($(echo "$PT_SERVERS" | tr ',' '\n'))
+		for i in "${!SERVERS[@]}"
+		do
+			echo "Waiting for ${SERVERS[i]}..."
+			until mysql --no-defaults -h "${SERVERS[i]}" -u "$PT_USER" -p"$PT_PASSWORD" "$PT_DATABASE" -nsLNE -e 'exit'; do
+				>&2 echo "MySQL Master is unavailable - sleeping"
+				sleep 5
+			done
+		done
+	fi
 
-  until mysql --no-defaults -h "$PT_MASTER" -u "$PT_USER" -p"$PT_PASSWORD" "$PT_DATABASE" -nsLNE -e 'exit'; do
-    >&2 echo "MySQL Master is unavailable - sleeping"
-    sleep 5
-  done
-  
+	until mysql --no-defaults -h "$PT_MASTER" -u "$PT_USER" -p"$PT_PASSWORD" "$PT_DATABASE" -nsLNE -e 'exit'; do
+		>&2 echo "MySQL Master is unavailable - sleeping"
+		sleep 5
+	done
+	
 
-  pt-heartbeat -D $PT_DATABASE --update -h $PT_MASTER --user $PT_USER --password $PT_PASSWORD
-  while [ $? -ne 0 ]; do
-    pt-heartbeat -D $PT_DATABASE --update -h $PT_MASTER --user $PT_USER --password $PT_PASSWORD
-  done
+	pt-heartbeat -D $PT_DATABASE --update -h $PT_MASTER --user $PT_USER --password $PT_PASSWORD
+	while [ $? -ne 0 ]; do
+		pt-heartbeat -D $PT_DATABASE --update -h $PT_MASTER --user $PT_USER --password $PT_PASSWORD
+	done
 
 fi
 
